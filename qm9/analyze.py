22fac190b8147ec2de343cb17abe92a7360ff591
carlportz <derlasse25@gmail.com>
2024-03-01 11:17:06 +0100

    Added support for PhotoDiff dataset

--- qm9/analyze.py
+++ qm9/analyze.py
@@ -227,8 +227,7 @@ def check_stability(positions, atom_type, dataset_info, debug=False):
                     or dataset_info['name'] == 'qm9_first_half' or dataset_info['name'] == 'PhotoDiff':
                 order = bond_analyze.get_bond_order(atom1, atom2, dist)
             elif dataset_info['name'] == 'geom':
-                order = bond_analyze.geom_predictor(
-                    (atom_decoder[pair[0]], atom_decoder[pair[1]]), dist):
+                order = bond_analyze.geom_predictor((atom_decoder[pair[0]], atom_decoder[pair[1]]), dist)
             nr_bonds[i] += order
             nr_bonds[j] += order
     nr_stable_bonds = 0
