d57dba43031f03bce0e942dee2de9d52c0914eb1
carlportz <derlasse25@gmail.com>
2024-03-01 11:41:19 +0100

    Add support for PhotoDiff dataset

--- qm9/rdkit_functions.py
+++ qm9/rdkit_functions.py
@@ -176,7 +176,8 @@ def build_xae_molecule(positions, atom_types, dataset_info):
     for i in range(n):
         for j in range(i):
             pair = sorted([atom_types[i], atom_types[j]])
-            if dataset_info['name'] == 'qm9' or dataset_info['name'] == 'qm9_second_half' or dataset_info['name'] == 'qm9_first_half':
+            if dataset_info['name'] == 'qm9' or dataset_info['name'] == 'qm9_second_half' \
+                    or dataset_info['name'] == 'qm9_first_half' or dataset_info['name'] == 'PhotoDiff':
                 order = get_bond_order(atom_decoder[pair[0]], atom_decoder[pair[1]], dists[i, j])
             elif dataset_info['name'] == 'geom':
                 order = geom_predictor((atom_decoder[pair[0]], atom_decoder[pair[1]]), dists[i, j], limit_bonds_to_one=True)
